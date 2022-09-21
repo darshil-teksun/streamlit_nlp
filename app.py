f3bc5015b4de4c2e171065a859ed10f30d924bf8
darshil-teksun <84508601+darshil-teksun@users.noreply.github.com>
2022-09-21 14:26:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,3 +40,21 @@ def run_model(input_text):
 
 if st.button('Submit'):
     run_model(text)
+    
+    
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("A cat")
+   st.markdown('Powered by Think In Bytes')
+   #st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.markdown('Powered by Think In Bytes')
+   #st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.markdown('Powered by Think In Bytes')
+   #st.image("https://static.streamlit.io/examples/owl.jpg")
